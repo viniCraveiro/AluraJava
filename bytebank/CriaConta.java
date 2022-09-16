1465b3b7c199cd6b23f83f02d5123bc57d162be1
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2022-09-16 00:20:34 -0300

    Criar conta

--- bytebank/CriaConta.java
+++ bytebank/CriaConta.java
@@ -4,19 +4,17 @@ public class CriaConta {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
-        System.out.println("Primeira Conta");
-        System.out.println(primeiraConta.saldo);
+        System.out.println("\nPrimeira Conta\n" +
+                "Saldo: "+primeiraConta.saldo);
 
         primeiraConta.saldo += 100;
-        System.out.println(primeiraConta.saldo);
+        System.out.println("\nPrimeira Conta\n" +
+                "Saldo Atual: "+primeiraConta.saldo);
 
-        Conta segundaConta = new Conta();
+        Conta segundaConta = primeiraConta;
         segundaConta.saldo = 50;
-        System.out.println("Segunda Conta");
-        System.out.println(segundaConta.saldo);
 
-
-        System.out.println("Primeira Conta tem o saldo de R$: " + primeiraConta.saldo);
+        System.out.println("\nPrimeira Conta tem o saldo de R$: " + primeiraConta.saldo);
         System.out.println("Segunda conta tem o saldo de R$: "+ segundaConta.saldo);
 
         System.out.println(primeiraConta.agencia);
